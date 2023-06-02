c8ebc17c898ea807c51d53ac6395fb4e67ae1252
Edenilson-Molina <Edenilsonmolina2001@hotmail.com>
2023-06-02 17:06:53 -0600

    Home: contador de pasos

--- app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/home/HomeFragment.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/home/HomeFragment.java
@@ -1,37 +1,108 @@
 package sv.edu.ues.fia.eisi.uesrunning.ui.home;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import sv.edu.ues.fia.eisi.uesrunning.R;
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
+//import sv.edu.ues.fia.eisi.uesrunning.databinding.FragmentHomeBinding;
 
-import sv.edu.ues.fia.eisi.uesrunning.databinding.FragmentHomeBinding;
+public class HomeFragment extends Fragment implements SensorEventListener {
 
-public class HomeFragment extends Fragment {
+    // SENSORES
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    // PARA LAS VISTAS
+    private CircularProgressBar progressBar;
+    private TextView contadortxt;
+    private Button bt;
+    int currentProgress;
+    private boolean sensorsEnabled = true;
 
-    private FragmentHomeBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Disparar el layout de home
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        // Inicializando variables
+        sensorManager = (SensorManager) requireContext().getSystemService(Context.SENSOR_SERVICE);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        progressBar = view.findViewById((R.id.progress_circular));
+        progressBar.setProgressMax(10000); // Pasos recomendados a diario
+        contadortxt = view.findViewById(R.id.step_counter);
+        currentProgress = -1;
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        bt = view.findViewById(R.id.toggleButton);
+        bt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                toggleSensors(v);
+            }
+        });
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return view;
+    }
+
+    private void setProgress(int progress) {
+        progressBar.setProgress(progress);
+    }
+
+    public void toggleSensors(View view) {
+        if (sensorsEnabled) {
+            sensorManager.unregisterListener(this);
+            sensorsEnabled = false;
+            bt.setText("Activar");
+        } else {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorsEnabled = true;
+            currentProgress -= 1;
+            bt.setText("Desactivar");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            float steps = event.values[0];
+            // Actualiza tu contador de pasos en la interfaz de usuario
+            // Actualiza el progreso
+            // Obtén el progreso actual de tus datos o cálculos
+            currentProgress += 1;
+            contadortxt.setText(String.valueOf(currentProgress));
+            setProgress(currentProgress);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Método requerido por la interfaz SensorEventListener,
+        // no necesitas implementarlo en este caso.
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
+
+
+
+
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/temporizador/TemporizadorFragment.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/temporizador/TemporizadorFragment.java
@@ -5,33 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import sv.edu.ues.fia.eisi.uesrunning.R;
 import sv.edu.ues.fia.eisi.uesrunning.databinding.FragmentTemporizadorBinding;
 
 public class TemporizadorFragment extends Fragment {
 
-    private FragmentTemporizadorBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_temporizador, container, false);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        TemporizadorViewModel temporizadorViewModel =
-                new ViewModelProvider(this).get(TemporizadorViewModel.class);
-
-        binding = FragmentTemporizadorBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        temporizadorViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+
     }
 }
