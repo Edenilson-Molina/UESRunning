c5ba63f8d02bd48feac7e4374bd1e6774ce77472
Tomas7855 <tomasromero867@gmail.com>
2023-06-02 17:34:57 -0600

    Agregar calendario

--- app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/misdatos/CalendarioFragment.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/misdatos/CalendarioFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import sv.edu.ues.fia.eisi.uesrunning.R;
 
@@ -18,5 +19,7 @@ public class CalendarioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendario, container, false);
+
+
     }
 }
