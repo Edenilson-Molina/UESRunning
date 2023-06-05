09554545f41773e14771a7d332e3bcca8239e6ba
abner-rivas <abnerrivas507@gmail.com>
2023-06-05 08:25:52 -0600

    YA SIRVE

--- app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/temporizador/TemporizadorFragment.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/uesrunning/ui/temporizador/TemporizadorFragment.java
@@ -1,30 +1,68 @@
 package sv.edu.ues.fia.eisi.uesrunning.ui.temporizador;
 
+import android.media.Ringtone;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import android.media.RingtoneManager;
+import android.os.CountDownTimer;
+import android.webkit.*;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
 
 import sv.edu.ues.fia.eisi.uesrunning.R;
 import sv.edu.ues.fia.eisi.uesrunning.databinding.FragmentTemporizadorBinding;
 
 public class TemporizadorFragment extends Fragment {
+    private ProgressBar pbCarga;
 
+    private EditText txtTiempo;
+    private TextView tvCuentaAtras;
+    private Button button;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_temporizador, container, false);
+        pbCarga = view.findViewById(R.id.pbCarga);
+        txtTiempo = view.findViewById(R.id.txt_tiempo);
+        tvCuentaAtras = view.findViewById(R.id.tv_cuentaatras);
 
+        button = view.findViewById(R.id.iniciar);
+        button.setOnClickListener(this::play);
         return view;
     }
 
+    public void play(View view) {
+        long tiempoSegundos = Long.parseLong(txtTiempo.getText().toString());
+        long tiempoMilisegundos = tiempoSegundos * 1000;
+        new CountDownTimer(tiempoMilisegundos, 1000) {
+            @Override
+            public void onFinish() {
+                Uri notificacion = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+                Ringtone r = RingtoneManager.getRingtone(getActivity(), notificacion);
+                r.play();
+                this.cancel();
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int tiempoSegundos = (int) (millisUntilFinished / 1000) + 1;
+                tvCuentaAtras.setText(String.format("%02d", tiempoSegundos));
+            }
+        }.start();
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
